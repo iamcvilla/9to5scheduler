8db8529845b9488d91bee6811e43e7b54fef7ef2
calvin <cal.villa@icloud.com>
2021-11-28 20:56:55 -0500

    set local storage

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // displays today's date
-$(document).ready(function() {
-
-    $("#currentDay").text(moment().format('ddd MMM Do, YYYY') );
-
+$(document).ready(function () {
+    $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 });
+
+//assign saveBtn click listener
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    //save items in local storage.
+    localStorage.setItem(time, text);
+})
+
+//load any saved data from LocalStorage
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
